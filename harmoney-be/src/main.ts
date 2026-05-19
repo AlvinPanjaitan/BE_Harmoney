5203e430d840f685d92137ef0bfacb619d6724e6
AlvinPanjaitan <alvinpanjaitan13@gmail.com>
2026-05-20 02:55:44 +0700

    Setup Vercel Deploy

--- harmoney-be/src/main.ts
+++ harmoney-be/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express = require('express');
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+const server = express();
+
+async function createServer() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors();
+  await app.init();
 }
-bootstrap();
+
+createServer();
+
+export default server;
